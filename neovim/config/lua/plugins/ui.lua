dd175a1ac54090525c42731cfe7b5317c0242c1a
Kory Taborn <kory.taborn@gmail.com>
2024-05-29 13:07:26 -0500

    neovim: reconfigure indentscope plugin

--- neovim/config/lua/plugins/ui.lua
+++ neovim/config/lua/plugins/ui.lua
@@ -71,31 +71,11 @@ return {
   {
     "echasnovski/mini.indentscope",
     opts = {
-      symbol = "│",
-      options = { try_as_border = true },
-    },
-    init = function()
-      vim.g.miniindentscope_disable = true
-      require("mini.indentscope").undraw()
-
-      -- Disable guide line for certain file types and plugins
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "help",
-          "alpha",
-          "dashboard",
-          "neo-tree",
-          "Trouble",
-          "lazy",
-          "mason",
-          "notify",
-          "toggleterm",
-          "lazyterm",
-        },
-        callback = function()
-          vim.b.miniindentscope_disable = true
+      draw = {
+        animation = function()
+          return 0
         end,
-      })
-    end,
+      },
+    },
   },
 }
