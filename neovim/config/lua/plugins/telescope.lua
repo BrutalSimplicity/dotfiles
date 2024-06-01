30744ad8681be4141b224787019bde746824d651
Kory Taborn <kory.taborn@gmail.com>
2024-05-31 22:29:45 -0500

    nvim: Telescope customizations

--- neovim/config/lua/plugins/telescope.lua
+++ neovim/config/lua/plugins/telescope.lua
@@ -1,32 +1,74 @@
 return {
-
   {
     "nvim-telescope/telescope.nvim",
     keys = {
       {
         "<leader>lc",
+        require("lazyvim.util").telescope("find_files", {
+          cwd = vim.fn.stdpath("data") .. "/lazy/LazyVim",
+        }),
+        desc = "LazyVim Configuration (Find Files)",
+      },
+      {
+        "<leader>lC",
+        require("lazyvim.util").telescope("live_grep", {
+          cwd = vim.fn.stdpath("data") .. "/lazy/LazyVim",
+        }),
+        desc = "LazyVim Configuration (Grep)",
+      },
+      {
+        "<leader>lp",
+        require("lazyvim.util").telescope("find_files", {
+          cwd = vim.fn.stdpath("data") .. "/lazy",
+        }),
+        desc = "LazyVim Plugins (Find Files)",
+      },
+      {
+        "<leader>fd",
         function()
           require("telescope.builtin").find_files({
-            cwd = vim.fn.stdpath("data") .. "/lazy/LazyVim",
+            cwd = vim.fs.dirname(vim.api.nvim_buf_get_name(0)),
           })
         end,
-        desc = "LazyVim Configuration",
+        desc = "Find Files (Buffer Dir)",
       },
     },
     dependencies = {
-      "nvim-lua/plenary.nvim",
       {
         "prochri/telescope-all-recent.nvim",
         dependencies = {
           "kkharji/sqlite.lua",
         },
       },
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-        cond = function()
-          return vim.fn.executable("make") == 1
-        end,
+    },
+    opts = {
+      defaults = {
+        prompt_prefix = "> ",
+        selection_caret = "> ",
+        layout_strategy = "horizontal",
+        winblend = 20,
+        layout_config = {
+          height = 0.5,
+        },
+        -- Cache the first 1000 results of all pickers
+        cache_pickers = {
+          num_pickers = -1,
+          limit_entries = 1000,
+        },
+        preview = {
+          hide_on_startup = true,
+        },
+        mappings = {
+          n = {
+            ["<c-p>"] = require("telescope.actions.layout").toggle_preview,
+          },
+          i = {
+            ["<c-u>"] = { "<c-s-u>", type = "command" },
+            ["<c-j>"] = require("telescope.actions").move_selection_next,
+            ["<c-k>"] = require("telescope.actions").move_selection_previous,
+            ["<c-p>"] = require("telescope.actions.layout").toggle_preview,
+          },
+        },
       },
     },
     config = function(_, opts)
