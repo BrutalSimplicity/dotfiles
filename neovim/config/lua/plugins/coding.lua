dab7313bb7a648738712198365ef2734113d8c5b
Kory Taborn <kory.taborn@gmail.com>
2024-05-31 22:26:49 -0500

    nvim: Add coding and rename utils -> general

---
+++ neovim/config/lua/plugins/coding.lua
@@ -0,0 +1,26 @@
+return {
+  -- Auto-completion(LazyVim)
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+      })
+      return opts
+    end,
+  },
+
+  -- Supplies .gitignore templates
+  {
+    "wintermute-cell/gitignore.nvim",
+    keys = {
+      {
+        "<leader>gi",
+        "<cmd>Gitignore<cr>",
+        desc = "Git Ignore",
+      },
+    },
+  },
+}
